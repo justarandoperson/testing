c3fe68f2672bfca2058cc72886ad9e7005026ac8
JustaCat <mister.bauer2015@yandex.ru>
2022-07-19 19:23:30 +0500

    Init commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout << "Greatest programm you have ever seen!\n";
+    for (int i = 0;;i++) {
+        SetConsoleTextAttribute(out, i);
+        std::cout << "Text\n";
+        if (i == 255) {
+            i = 0;
+        }
+    }
+}
